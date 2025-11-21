493b3c8b349854662c054c4b18045ae42322d2a0
FlipFlop17 <42313405+FlipFlop17@users.noreply.github.com>
2025-11-21 17:43:53 +0100

    fix blob middleware and other

---
+++ AzRebit.FunctionExample/Features/TransferCats_BlobTrigger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AzRebit.HelperExtensions;
+
+using Azure.Storage.Blobs;
+
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AzRebit.FunctionExample.Features;
+
+public class TransferCats_BlobTrigger
+{
+    private readonly ILogger<TransferCats_BlobTrigger> _logger;
+    private bool deleteResubmitionFile = Environment.GetEnvironmentVariable("AZREBIT_DELETE_RESUBMITION_FILE") == "true";
+    public TransferCats_BlobTrigger(ILogger<TransferCats_BlobTrigger> logger)
+    {
+        _logger = logger;
+    }
+
+
+    /// <summary>
+    /// Blob trigger example
+    /// </summary>
+    /// <param name="req"></param>
+    /// <returns></returns>
+    [Function("TransferCats")]
+    public async Task RunAdd(
+        [BlobTrigger("cats-container/{blobPath}", Connection = "AzureWebJobsStorage")] BlobClient blobClient, string blobPath,
+        FunctionContext funcContext)
+    {
+        _logger.LogInformation("incoming payload saved");
+        Console.WriteLine(blobClient.Name);
+        //optional but recomended - if processing was successfull delete the file as we won't need it for resubmition
+        if (deleteResubmitionFile)
+            await AzRebitBlobExtensions.DeleteSavedResubmitionBlobAsync(funcContext.InvocationId.ToString());
+
+        //publish the finished work on service bus, event, external endpoint etc.
+    }
+
+    /// <summary>
+    /// Blob trigger example
+    /// </summary>
+    /// <param name="req"></param>
+    /// <returns></returns>
+    [Function("TransferCatsAnotherTrigger")]
+    public async Task RunAnother(
+        [BlobTrigger("cats-container-2/{blobPath}")] BlobClient blobClient, string blobPath,
+        FunctionContext funcContext)
+    {
+        _logger.LogInformation("incoming payload saved");
+        Console.WriteLine(blobClient.Name);
+        //optional but recomended - if processing was successfull delete the file as we won't need it for resubmition
+        if (deleteResubmitionFile)
+            await AzRebitBlobExtensions.DeleteSavedResubmitionBlobAsync(funcContext.InvocationId.ToString());
+
+        //publish the finished work on service bus, event, external endpoint etc.
+    }
+}
